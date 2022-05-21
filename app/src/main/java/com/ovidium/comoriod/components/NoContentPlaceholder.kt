9ac2a7cac8c3c64ccdaf1e25cd8baecb405369eb
Mugurel Moscaliuc <mugur.moscaliuc@gmail.com>
2022-05-21 10:04:12 +0100

    NoContentPlaceholder

---
+++ app/src/main/java/com/ovidium/comoriod/components/NoContentPlaceholder.kt
@@ -0,0 +1,28 @@
+package com.ovidium.comoriod.components
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+
+@Composable
+fun NoContentPlaceholder(message: String) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        Text(
+            text = message,
+            style = MaterialTheme.typography.h6,
+            color = if (isSystemInDarkTheme()) Color.DarkGray else Color.LightGray
+        )
+    }
+}
