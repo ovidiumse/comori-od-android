c4379ad00a65f3d9c73a871432e2b81fb676cb08
Ovidiu Moscaliuc <moscaliuc_ovidiu@yahoo.com>
2022-12-30 15:18:36 +0200

    Improved autocomplete items representation

--- app/src/main/java/com/ovidium/comoriod/views/search/AutocompleteCell.kt
+++ app/src/main/java/com/ovidium/comoriod/views/search/AutocompleteCell.kt
@@ -2,18 +2,18 @@ package com.ovidium.comoriod.views.search
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.Divider
 import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
-import androidx.navigation.NavHostController
 import com.ovidium.comoriod.R
 import com.ovidium.comoriod.data.autocomplete.Hit
 import com.ovidium.comoriod.ui.theme.getNamedColor
@@ -24,46 +24,49 @@ import java.net.URLEncoder
 @Composable
 fun AutocompleteCell(hit: Hit, navController: NavController, isDark: Boolean) {
     val textColor = getNamedColor("Text", isDark)
+    val mutedTextColor = getNamedColor("MutedText", isDark)
 
-    Column {
+    Column(modifier = Modifier
+        .clickable {
+            navController.navigate(
+                Screens.Article.withArgs(
+                    URLEncoder.encode(
+                        hit._id,
+                        "utf-8"
+                    )
+                )
+            ) {
+                launchSingleTop = true
+            }
+        }) {
         Text(
             text = hit._source.title,
-            color = textColor,
-            fontSize = 16.sp,
-            modifier = Modifier
-                .padding(vertical = 3.dp)
-                .clickable {
-                    navController.navigate(
-                        Screens.Article.withArgs(
-                            URLEncoder.encode(
-                                hit._id,
-                                "utf-8"
-                            )
-                        )
-                    ) {
-                        launchSingleTop = true
-                    }
-                }
+            style = MaterialTheme.typography.body1,
+            fontWeight = FontWeight.Bold,
+            color = textColor
         )
         Row(
             verticalAlignment = Alignment.CenterVertically,
-            modifier = Modifier.fillMaxWidth()
+            modifier = Modifier.fillMaxWidth().padding(top=2.dp)
         ) {
             Icon(
                 modifier = Modifier.size(12.dp),
                 imageVector = ImageVector.vectorResource(id = R.drawable.ic_book),
-                contentDescription = "Some icon",
-                tint = Color.Red
+                contentDescription = "Book icon",
+                tint = mutedTextColor.copy(alpha=0.7f)
             )
             Spacer(modifier = Modifier.width(5.dp))
             Text(
-                text = hit._source.book,
-                fontSize = 12.sp,
-                color = Color.Gray,
-                modifier = Modifier
-                    .padding(vertical = 3.dp)
+                text = "${hit._source.author} - ${hit._source.book}",
+                style = MaterialTheme.typography.subtitle2,
+                color = mutedTextColor
             )
         }
-        Spacer(modifier = Modifier.height(10.dp))
+
+        Divider(
+            color = mutedTextColor.copy(alpha = 0.5f),
+            thickness = 0.3.dp,
+            modifier = Modifier.padding(vertical = 8.dp)
+        )
     }
 }
