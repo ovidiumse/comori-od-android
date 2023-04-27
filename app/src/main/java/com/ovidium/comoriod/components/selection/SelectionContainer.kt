a7c08f0fcc7c809a2f12d56b101907475d81fb60
Ovidiu Moscaliuc <moscaliuc_ovidiu@yahoo.com>
2023-04-27 16:26:37 +0300

    Updated compose to 1.4.2, and refactored SelectionContainer for text selection support

--- app/src/main/java/com/ovidium/comoriod/components/selection/SelectionContainer.kt
+++ app/src/main/java/com/ovidium/comoriod/components/selection/SelectionContainer.kt
@@ -2,16 +2,28 @@
 
 package com.ovidium.comoriod.components.selection
 
-import androidx.compose.foundation.text.detectDragGesturesWithObserver
+import androidx.compose.foundation.text.ContextMenuArea
+import androidx.compose.foundation.text.detectDownAndDragGesturesWithObserver
 import androidx.compose.foundation.text.isInTouchMode
 import androidx.compose.foundation.text.selection.*
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalClipboardManager
 import androidx.compose.ui.platform.LocalHapticFeedback
 import androidx.compose.ui.platform.LocalTextToolbar
 
+/**
+ * Enables text selection for its direct or indirect children.
+ *
+ * @sample androidx.compose.foundation.samples.SelectionSample
+ */
 @Composable
 fun SelectionContainer(
     modifier: Modifier = Modifier,
@@ -41,6 +53,27 @@ fun SelectionContainer(
     )
 }
 
+/**
+ * Disables text selection for its direct or indirect children. To use this, simply add this
+ * to wrap one or more text composables.
+ *
+ * @sample androidx.compose.foundation.samples.DisableSelectionSample
+ */
+@Composable
+fun DisableSelection(content: @Composable () -> Unit) {
+    CompositionLocalProvider(
+        LocalSelectionRegistrar provides null,
+        content = content
+    )
+}
+
+/**
+ * Selection Composable.
+ *
+ * The selection composable wraps composables and let them to be selectable. It paints the selection
+ * area with start and end handles.
+ */
+@Suppress("ComposableLambdaParameterNaming")
 @Composable
 internal fun SelectionContainer(
     /** A [Modifier] for SelectionContainer. */
@@ -60,45 +93,45 @@ internal fun SelectionContainer(
     manager.onSelectionChange = onSelectionChange
     manager.selection = selection
     manager.touchMode = isInTouchMode
-
     if (selection?.start?.offset == null)
         manager.onRelease()
 
-    CompositionLocalProvider(LocalSelectionRegistrar provides registrarImpl) {
-        // Get the layout coordinates of the selection container. This is for hit test of
-        // cross-composable selection.
-        SimpleLayout(modifier = modifier.then(manager.modifier)) {
-            children()
-            if (isInTouchMode && manager.hasFocus) {
-                manager.selection?.let {
+    ContextMenuArea(manager) {
+        CompositionLocalProvider(LocalSelectionRegistrar provides registrarImpl) {
+            // Get the layout coordinates of the selection container. This is for hit test of
+            // cross-composable selection.
+            SimpleLayout(modifier = modifier.then(manager.modifier)) {
+                children()
+                if (isInTouchMode && manager.hasFocus) {
+                    manager.selection?.let {
+                        listOf(true, false).forEach { isStartHandle ->
+                            val observer = remember(isStartHandle) {
+                                manager.handleDragObserver(isStartHandle)
+                            }
+                            val position = if (isStartHandle) {
+                                manager.startHandlePosition
+                            } else {
+                                manager.endHandlePosition
+                            }
 
-                    listOf(true, false).forEach { isStartHandle ->
-                        val observer = remember(isStartHandle) {
-                            manager.handleDragObserver(isStartHandle)
-                        }
-                        val position = if (isStartHandle) {
-                            manager.startHandlePosition
-                        } else {
-                            manager.endHandlePosition
-                        }
+                            val direction = if (isStartHandle) {
+                                it.start.direction
+                            } else {
+                                it.end.direction
+                            }
 
-                        val direction = if (isStartHandle) {
-                            it.start.direction
-                        } else {
-                            it.end.direction
-                        }
-
-                        if (position != null) {
-                            SelectionHandle(
-                                position = position,
-                                isStartHandle = isStartHandle,
-                                direction = direction,
-                                handlesCrossed = it.handlesCrossed,
-                                modifier = Modifier.pointerInput(observer) {
-                                    detectDragGesturesWithObserver(observer)
-                                },
-                                content = null
-                            )
+                            if (position != null) {
+                                SelectionHandle(
+                                    position = position,
+                                    isStartHandle = isStartHandle,
+                                    direction = direction,
+                                    handlesCrossed = it.handlesCrossed,
+                                    modifier = Modifier.pointerInput(observer) {
+                                        detectDownAndDragGesturesWithObserver(observer)
+                                    },
+                                    content = null
+                                )
+                            }
                         }
                     }
                 }
